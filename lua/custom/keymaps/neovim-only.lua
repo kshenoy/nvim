d61c5af01822c19d0e2eb6f9244d35deb70be587
Kartik Shenoy <theresmorethanoneway@gmail.com>
2024-11-29 15:54:12 -0800

    Disabled use of plug for keymaps as I don't know how to get it to play nicely with which-key

--- lua/custom/keymaps/neovim-only.lua
+++ lua/custom/keymaps/neovim-only.lua
@@ -8,7 +8,7 @@ map('n', '<C-G>', '2<C-G>')
 map('n', 'ZQ', '<Cmd>qall!<CR>')
 
 -- Copy the file name to unix visual select buffer
-map('n', '<leader>fy', function()
+map('n', '<Leader>fy', function()
   vim.cmd('let @+="' .. vim.fn.expand '%:p' .. '"')
 end, { desc = 'Copy file path' })
 
@@ -17,14 +17,14 @@ end, { desc = 'Copy file path' })
 local fzf_lua_p4 = require('neovim-only.fzf-lua.perforce')
 
 -- Checkout the file if in a VCS
-map('n', '<Plug>(leader-vcs-map)e', function()
+map('n', '<Leader>ve', function()
   if fzf_lua_p4.is_p4_repo({}, true) then
     vim.cmd "!p4 edit %"
   end
 end, {desc = "Checkout file"})
 
 
-map('n', '<Plug>(leader-vcs-map)s', function()
+map('n', '<Leader>vs', function()
   if require('fzf-lua.path').is_git_repo({}, true) then
     return require('fzf-lua').git_status()
   elseif fzf_lua_p4.is_p4_repo({}, true) then
@@ -35,7 +35,7 @@ end, {desc="Repo status"})
 --
 
 -- [[ MISC ]] ----------------------------------------------------------------------------------------------------------
-map('n', '<leader>kl', function()
+map('n', '<Leader>kl', function()
   if (vim.fn.getloclist(0, { winid = 0 }).winid or 0) == 0 then
     vim.cmd 'lopen'
   else
@@ -43,7 +43,7 @@ map('n', '<leader>kl', function()
   end
 end, { desc = 'Toggle LocationList' })
 
-map('n', '<leader>kq', function()
+map('n', '<Leader>kq', function()
   for _, win in pairs(vim.fn.getwininfo()) do
     if (win.quickfix == 1) and (win.loclist == 0) then
       vim.cmd 'cclose'
@@ -53,13 +53,8 @@ map('n', '<leader>kq', function()
   vim.cmd 'copen'
 end, { desc = 'Toggle QuickFix' })
 
-
 -- Window resize (respecting `v:count`)
-map('n', '<C-S-Left>',  '"<Cmd>vertical resize -" . v:count1 . "<CR>"',
-  { expr = true, replace_keycodes = false, desc = 'Decrease window width' })
-map('n', '<C-S-Down>',  '"<Cmd>resize -"          . v:count1 . "<CR>"',
-  { expr = true, replace_keycodes = false, desc = 'Decrease window height' })
-map('n', '<C-S-Up>',    '"<Cmd>resize +"          . v:count1 . "<CR>"',
-  { expr = true, replace_keycodes = false, desc = 'Increase window height' })
-map('n', '<C-S-Right>', '"<Cmd>vertical resize +" . v:count1 . "<CR>"',
-  { expr = true, replace_keycodes = false, desc = 'Increase window width' })
+map('n', '<C-S-Left>', '"<Cmd>vertical resize -" . 3*v:count1 . "<CR>"', { expr = true, replace_keycodes = false, desc = 'Decrease window width' })
+map('n', '<C-S-Right>', '"<Cmd>vertical resize +" . 3*v:count1 . "<CR>"', { expr = true, replace_keycodes = false, desc = 'Increase window width' })
+map('n', '<C-S-Down>', '"<Cmd>resize -" . 2*v:count1 . "<CR>"', { expr = true, replace_keycodes = false, desc = 'Decrease window height' })
+map('n', '<C-S-Up>', '"<Cmd>resize +" . 2*v:count1 . "<CR>"', { expr = true, replace_keycodes = false, desc = 'Increase window height' })
