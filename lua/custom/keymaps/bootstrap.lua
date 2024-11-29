210bbcdeff8059a2b64ebda2fba5721a11d7d91d
Kartik Shenoy <theresmorethanoneway@gmail.com>
2024-11-29 15:54:12 -0800

    Disabled use of plug for keymaps as I don't know how to get it to play nicely with which-key

--- lua/custom/keymaps/bootstrap.lua
+++ lua/custom/keymaps/bootstrap.lua
@@ -1,30 +1,32 @@
--- Set <space> as the leader key. See `:help mapleader`
+-- Set <space> as the Leader key. See `:help mapleader`
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
 -- Create some keymaps early so that they can be used by plugins
-vim.keymap.set({'n', 'v'}, '<Leader>b', '<Plug>(leader-buffer-map)',     {remap=true, silent=true})
-vim.keymap.set({'n', 'v'}, '<Leader>c', '<Plug>(leader-code-map)',       {remap=true, silent=true})
-vim.keymap.set({'n', 'v'}, '<Leader>f', '<Plug>(leader-file-map)',       {remap=true, silent=true})
-vim.keymap.set({'n', 'v'}, '<Leader>g', '<Plug>(leader-goto-map)',       {remap=true, silent=true})
-vim.keymap.set({'n', 'v'}, '<Leader>h', '<Plug>(leader-help-map)',       {remap=true, silent=true})
+if not require('custom.utils').is_neovim() then -- disabling it for neovim as it breaks which-key
+  vim.keymap.set({ 'n', 'v' }, '<Leader>b', '<Plug>(leader-buffer-map)', { remap = true, silent = true })
+  vim.keymap.set({ 'n', 'v' }, '<Leader>c', '<Plug>(leader-code-map)', { remap = true, silent = true })
+  vim.keymap.set({ 'n', 'v' }, '<Leader>f', '<Plug>(leader-file-map)', { remap = true, silent = true })
+  vim.keymap.set({ 'n', 'v' }, '<Leader>g', '<Plug>(leader-goto-map)', { remap = true, silent = true })
+  vim.keymap.set({ 'n', 'v' }, '<Leader>h', '<Plug>(leader-help-map)', { remap = true, silent = true })
 
--- Searching for editor-specific things go here eg. searching quickfix, locationlist etc.
--- Searching for things related to the code go in the Search keymap
-vim.keymap.set({'n', 'v'}, '<Leader>i', '<Plug>(leader-info-map)',       {remap=true, silent=true})
+  -- Searching for editor-specific things go here eg. searching quickfix, locationlist etc.
+  -- Searching for things related to the code go in the Search keymap
+  vim.keymap.set({ 'n', 'v' }, '<Leader>i', '<Plug>(leader-info-map)', { remap = true, silent = true })
 
--- This is for editor-specific settings. Editor-agnostic settings should go into the Toggle map
-vim.keymap.set({'n', 'v'}, '<Leader>k', '<Plug>(leader-kustomize-map)',  {remap=true, silent=true})
+  -- This is for editor-specific settings. Editor-agnostic settings should go into the Toggle map
+  vim.keymap.set({ 'n', 'v' }, '<Leader>k', '<Plug>(leader-kustomize-map)', { remap = true, silent = true })
 
-vim.keymap.set({'n', 'v'}, '<Leader>o', '<Plug>(leader-open-map)',       {remap=true, silent=true})
-vim.keymap.set({'n', 'v'}, '<Leader>p', '<Plug>(leader-project-map)',    {remap=true, silent=true})
+  vim.keymap.set({ 'n', 'v' }, '<Leader>o', '<Plug>(leader-open-map)', { remap = true, silent = true })
+  vim.keymap.set({ 'n', 'v' }, '<Leader>p', '<Plug>(leader-project-map)', { remap = true, silent = true })
 
--- Searching for code-specific things go here.
--- Searching for things related to the editor go in the Info keymap eg. quickfix, locationlist etc.
-vim.keymap.set({'n', 'v'}, '<Leader>s', '<Plug>(leader-search-map)',     {remap=true, silent=true})
+  -- Searching for code-specific things go here.
+  -- Searching for things related to the editor go in the Info keymap eg. quickfix, locationlist etc.
+  vim.keymap.set({ 'n', 'v' }, '<Leader>s', '<Plug>(leader-search-map)', { remap = true, silent = true })
 
--- This is for editor-agnostic settings. Editor-specific settings should go into the Kustom map
-vim.keymap.set({'n', 'v'}, '<Leader>t', '<Plug>(leader-toggle-map)',     {remap=true, silent=true})
+  -- This is for editor-agnostic settings. Editor-specific settings should go into the Kustom map
+  vim.keymap.set({ 'n', 'v' }, '<Leader>t', '<Plug>(leader-toggle-map)', { remap = true, silent = true })
 
-vim.keymap.set({'n', 'v'}, '<Leader>v', '<Plug>(leader-vcs-map)',        {remap=true, silent=true})
-vim.keymap.set({'n', 'v'}, '<Leader>w', '<Plug>(leader-window-map)',     {remap=true, silent=true})
+  vim.keymap.set({ 'n', 'v' }, '<Leader>v', '<Plug>(leader-vcs-map)', { remap = true, silent = true })
+  vim.keymap.set({ 'n', 'v' }, '<Leader>w', '<Plug>(leader-window-map)', { remap = true, silent = true })
+end
--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -23,8 +23,8 @@ map('n', 'cW', 'dWi')
 
 -- BUFFERS -------------------------------------------------------------------------------------------------------------
 -- Switching buffers is something I do often, so make that as fast as possible
-map('n', '<Leader><Leader>', '<Plug>(leader-buffer-map)b', { remap = true, silent = true })
-map('n', '<Leader>,', '<cmd>b#<CR>', { silent = true })
+map('n', '<Leader><Leader>', '<Leader>bb', { remap = true, silent = true })
+map('n', '<Leader>,', '<cmd>b#<CR>', { silent = true, desc = 'Alternate buffer' })
 
 -- INDENTATION AND STYLING ---------------------------------------------------------------------------------------------
 -- Preserve visual block after indenting, increment/decrement
