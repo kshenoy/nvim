2c14aeb2cbd5847c7076f002d04590c2a52581c9
Kartik Shenoy <theresmorethanoneway@gmail.com>
2024-11-29 15:58:07 -0800

    Swapped leap and surround keybinds as I likely will use leap more frequently

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -2,10 +2,10 @@ return {
   'ggandor/leap.nvim',
 
   config = function()
-    vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-forward-to)', {remap=true})
-    vim.keymap.set({'n', 'x', 'o'}, 'gS', '<Plug>(leap-backward-to)', {remap=true})
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward-to)', { remap = true })
+    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward-to)', { remap = true })
     if not vim.g.vscode then
-      vim.keymap.set('n', 'g<C-s>', '<Plug>(leap-cross-window)', {remap=true})
+      vim.keymap.set('n', '<M-s>', '<Plug>(leap-from-window)', { remap = true })
     end
   end,
 }
